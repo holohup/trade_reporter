cc18e83ee4787b0e7147150f74b6e0d529d7f94e
Alexey Shapiro <academ@gmail.com>
2023-08-09 18:00:13 +0300

    migrated settings to settings.py, only 1 setting in .env now

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,13 +1,12 @@
 from aiogram import Bot
-import os
-from dotenv import load_dotenv, find_dotenv
+import settings
 
-load_dotenv(dotenv_path=find_dotenv())
-bot = Bot(os.getenv('TG_BOT_TOKEN'))
+bot = Bot(settings.get('TG_BOT_TOKEN'))
+chat_id = settings.get('TG_CHAT_ID')
 
 
 async def send_message(actor: str, msg: str) -> None:
     session = await bot.get_session()
     message = f'{actor}: {msg}'
-    await bot.send_message(os.getenv('TG_CHAT_ID'), message)
+    await bot.send_message(chat_id, message)
     await session.close()
