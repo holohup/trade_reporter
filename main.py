4774b5b5918535e08406181587008bf6f6ee628f
Alexey Shapiro <academ@gmail.com>
2023-08-09 18:56:58 +0300

    removed parsing logic from reporter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import asyncio
 from bot import send_message
-from tinkoff.invest import OrderTrades, OrderDirection
-from pickle import loads
-from repo import TCSAssetRepo
-from tinkoff.invest.utils import quotation_to_decimal
 import redis.asyncio as redis
 import logging
 import settings
@@ -13,35 +9,14 @@ logging.basicConfig(
 )
 
 
-def parse_tcs_trade(trades: OrderTrades) -> str:
-    repo = TCSAssetRepo()
-    ticker = repo[trades.instrument_uid].ticker
-    quantity = 0
-    total_price = 0
-    for trade in trades.trades:
-        quantity += trade.quantity
-        total_price += quotation_to_decimal(trade.price) * trade.quantity
-    if quantity == 0:
-        return f'Empty order report received: {trades}'
-    exec_price = float(total_price / quantity)
-    direction = (
-        'SOLD'
-        if trades.direction == OrderDirection.ORDER_DIRECTION_SELL
-        else 'BOUGHT'
-    )
-    details = f'{ticker} {direction} {quantity} for {exec_price}'
-    return f'TCS order filled: {details}'
-
-
 async def main():
     await send_message('trade reporter', 'Reporter operational')
-    r = redis.from_url(settings.get('TRADES_URL'))
+    r = redis.from_url(settings.get('MESSAGES_URL'), decode_responses=True)
     while True:
         async with r as client:
-            _, trades = await client.brpop('tcs_trades')
-            trade_report = parse_tcs_trade(loads(trades))
-            logging.info(f'trade report received: {trade_report}')
-            await send_message('trade reporter', trade_report)
+            _, msg = await client.brpop('messages')
+            logging.info(f'message received: {msg}')
+            await send_message('trade reporter', msg)
 
 
 if __name__ == '__main__':
